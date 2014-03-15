8f56f1ed46169dc772ad444064b02d43dc817608
James Owens <applemaster360@gmail.com>
2014-03-15 00:33:02 -0400

    Fixed saving currently selected toppings

--- RandomePizzaMarkII/RandomPizzaGeneratorToppingViewController.m
+++ RandomePizzaMarkII/RandomPizzaGeneratorToppingViewController.m
@@ -194,7 +194,10 @@
         [self.currentlySelectedToppings setObject:toppingCell.cellsTopping forKey:toppingCell.cellsTopping.name];
         
     }
-    [self prepareCurrentToppingsForSegue]; 
+    self.brain.toppingsPool = [self.currentlySelectedToppings copy];
+    [self.brain saveState];
+
+
     
 }
 
@@ -234,22 +237,7 @@
     }
 }
 
--(void)prepareCurrentToppingsForSegue
 
-{
-    /*
-    NSMutableArray *temp = [[NSMutableArray alloc]init];
-    for (NSString *key in self.currentlySelectedToppings) {
-        
-        [temp addObject:[self.currentlySelectedToppings objectForKey:key]];
-    }
-    self.brain.toppingsPool = [temp copy];
-    [self.brain saveState];
-    */
-    self.brain.toppingsPool = [self.currentlySelectedToppings copy];
-    
-    
-}
 -(void)makeIntoDicionary
 
 {
@@ -259,56 +247,5 @@
     }
 }
 
-#pragma mark commented out
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a story board-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-
- */
 
 @end
